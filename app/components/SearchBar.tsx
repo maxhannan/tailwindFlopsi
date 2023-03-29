a823f68ff55f30542b015d587a8a455f0b7553e0
maxhannan <maxhannan@substanceseven.com>
2023-03-29 14:52:36 -0500

    input

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -23,7 +23,6 @@ const SearchBar = () => {
           </svg>
         </div>
         <input
-          type="search"
           id="search"
           className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search"
