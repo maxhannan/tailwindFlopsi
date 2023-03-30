52c38d3cdcc11138e34c205c324c8f8de7c11567
maxhannan <maxhannan@substanceseven.com>
2023-03-29 21:33:09 -0500

    slide

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,12 +25,12 @@ export default function Index() {
           <Transition
             show={open}
             className=" z-40 grid grid-cols-2 gap-2 mb-1"
-            enter="transition ease-in duration-400"
-            enterFrom="opacity-0 "
-            enterTo="opacity-100  "
-            leave="transition ease-in duration-400"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter="transition-all  ease-in duration-500 overflow-hidden"
+            enterFrom="transform opacity-0 max-h-0"
+            enterTo="transform opacity-100 max-h-96"
+            leave="transition-all ease-out duration-500 overflow-hidden"
+            leaveFrom="transform opacity-100 max-h-96"
+            leaveTo="transform opacity-0 max-h-0"
           >
             <div className=" col-span-2">
               <SelectBox />
@@ -67,7 +67,7 @@ export default function Index() {
               <SelectBox />
             </div>
           </Transition>
-          <div className=" flex justify-between space-x-2 mt-2 ">
+          <div className=" flex justify-between space-x-2 ">
             <SearchBar />
             <button className="grow-2" onClick={() => setOpen(!open)}>
               <div className="relative inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-900 dark:hover:bg-gray-800 hover:bg-gray-200 group">
