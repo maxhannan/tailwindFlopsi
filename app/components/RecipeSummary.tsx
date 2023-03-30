06c5694542fea89b264dc96bc3fd4ba36214f0cd
maxhannan <maxhannan@substanceseven.com>
2023-03-29 22:40:36 -0500

    outlines

--- app/components/RecipeSummary.tsx
+++ app/components/RecipeSummary.tsx
@@ -1,6 +1,6 @@
 const RecipeSummary = () => {
   return (
-    <div className=" h-20 w-full bg-gray-50 rounded-md shadow-inner  flex justify-start  items-center px-4 hover:bg-gray-200 hover:bg-opacity-40 dark:bg-gray-800 dark:border-gray-600">
+    <div className=" h-20 w-full bg-gray-50 rounded-md  flex justify-start border border-gray-200 items-center px-4 hover:bg-gray-200 hover:bg-opacity-40 dark:bg-gray-800 dark:border-gray-600">
       <div className=" z-0  inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-blue-600 rounded-full dark:bg-gray-600">
         <span className="text-xl text-gray-100 dark:text-gray-300">JL</span>
       </div>
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -24,7 +24,7 @@ const SearchBar = () => {
         </div>
         <input
           id="search"
-          className="block shadow-inner  w-full p-4 pl-10 text-sm text-gray-900 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block   w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search"
           required
         />
