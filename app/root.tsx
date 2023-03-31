49f1648f6df4534697b111c21f4b01582c734253
maxhannan <maxhannan@substanceseven.com>
2023-03-30 22:30:52 -0500

    styling

--- app/root.tsx
+++ app/root.tsx
@@ -26,11 +26,8 @@ export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport:
-    "width=device-width,initial-scale=1, maximum-scale=1.0,user-scalable=0  viewport-fit=cover",
+    "width=device-width,initial-scale=1, maximum-scale=1.0,user-scalable=0 ",
   "apple-mobile-web-app-capable": "yes",
-
-  theme_color: "#f8fafc",
-  background_color: "#f8fafc",
 
   display: "standalone",
   "mobile-web-app-capable": "yes",
